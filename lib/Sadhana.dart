07cb4358440dd5cbd242fd0a88e58c702e99c35f
Harshal <harshalts@gmail.com>
2021-06-21 20:25:34 +0400

    Dunno why big space in alert dialog, but added alert dialog

--- lib/Sadhana.dart
+++ lib/Sadhana.dart
@@ -6,6 +6,45 @@ class Sadhana extends StatefulWidget {
 }
 
 class _SadhanaState extends State<Sadhana> {
+
+  final roundsCon = new TextEditingController();
+
+  static AlertDialog dialog = AlertDialog(
+    title: Text('Sadhana Information'),
+    //contentPadding: EdgeInsets.zero,
+    content: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter restaurant",
+
+              ),
+              //controller: roundsCon,
+              cursorColor: Colors.black,
+
+            ),
+          ],
+        ),
+    ),
+    actions: [
+      TextButton(
+        //textColor: Colors.red,
+        //onPressed: () => Navigator.pop(context),
+        onPressed: () {  },
+        child: Text('ACTION 1'),
+      ),
+      TextButton(
+        //textColor: Colors.red,
+        //onPressed: () => Navigator.pop(context),
+        child: Text('ACTION 2'),
+        onPressed: () {},
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +52,7 @@ class _SadhanaState extends State<Sadhana> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-
+          showDialog<void>(context: context, builder: (context) => dialog);
         },
       ),
     );
