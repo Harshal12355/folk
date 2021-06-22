0a2fe7f805cbe60ebbdf0bfc2c80d3bc9aee70ae
Harshal12355 <36196770+Harshal12355@users.noreply.github.com>
2021-06-22 14:10:11 +0400

    Fixed dialog

--- lib/screens/sadhana_screen.dart
+++ lib/screens/sadhana_screen.dart
@@ -7,38 +7,59 @@ class Sadhana extends StatefulWidget {
 }
 
 class _SadhanaState extends State<Sadhana> {
-  final roundsCon = new TextEditingController();
-
-  final AlertDialog dialog = AlertDialog(
-    title: Text('Sadhana Information'),
-    contentPadding: EdgeInsets.zero,
-    content: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(),
-        ],
-      ),
-    ),
-    actions: [
-      TextButton(
-        //textColor: Colors.red,
-        //onPressed: () => Navigator.pop(context),
-        onPressed: () {},
-        child: Text('Submit'),
-      ),
-    ],
-  );
-
+  final TextEditingController roundsCon = new TextEditingController();
+  final TextEditingController lecturesCon = new TextEditingController();
+  final TextEditingController linksCon = new TextEditingController();
   @override
   Widget build(BuildContext context) {
+    AlertDialog dialog = AlertDialog(
+      title: Text('Sadhana Information'),
+      contentPadding: EdgeInsets.zero,
+      content: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: roundsCon,
+              decoration: new InputDecoration(
+                hintText: "Number of rounds",
+              ),
+            ),
+            TextFormField(
+              controller: lecturesCon,
+              decoration: new InputDecoration(
+                hintText: "Time spent listening to lectures",
+              ),
+            ),
+            TextFormField(
+              controller: linksCon,
+              decoration: new InputDecoration(
+                hintText: "Links",
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          //textColor: Colors.red,
+          //onPressed: () => Navigator.pop(context),
+          onPressed: () {
+            _sendDataBack(context);
+          },
+          child: Text('Submit'),
+        ),
+      ],
+    );
+
     return Scaffold(
       body: ListView(children: <Widget>[
-        SadhanaCard(name: "Gaurav", rounds: 5, lectures: 2, links: <String>[
-          "https://github.com/Harshal12355/flashcards/blob/master/lib/Flashcard_View.dart",
-          "https://www.youtube.com/watch?v=tbGqmS15ALY"
-        ]),
+        for (int i = 0; i < 10; i++)
+          SadhanaCard(name: "Gaurav", rounds: 5, lectures: 2, links: <String>[
+            "https://github.com/Harshal12355/flashcards/blob/master/lib/Flashcard_View.dart",
+            "https://www.youtube.com/watch?v=tbGqmS15ALY"
+          ])
       ]),
       floatingActionButton: FloatingActionButton(
         child: Icon(
@@ -51,5 +72,14 @@ class _SadhanaState extends State<Sadhana> {
         },
       ),
     );
+  }
+
+  void _sendDataBack(BuildContext context) {
+    String roundsBack = roundsCon.text;
+    String lecturesBack = lecturesCon.text;
+    String linksBack = linksCon.text;
+    print(roundsBack);
+    print(lecturesBack);
+    print(linksBack);
   }
 }
